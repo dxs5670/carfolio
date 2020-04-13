8fa1b825256d70654dc660fb6215c1b9a82b0bd8
Caden Martz <cadenm7@gmail.com>
2020-04-13 12:40:42 -0400

    tried adding some empty FXMLs

---
+++ carfolio/Carfolio/src/view/view.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author caden
+ */
+public class view {
+    // abstract view class
+    
+}
