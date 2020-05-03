4af4a2ee716d77aa29bc7e86426a9caf1505170f
Dylan <53993453+dxs5670@users.noreply.github.com>
2020-05-03 15:31:48 -0400

    Added comments to main controller
    Added comments
    Added description
    Reformatted and adjusted presentation overall

--- carfolio/src/controller/mainController.java
+++ carfolio/src/controller/mainController.java
@@ -1,8 +1,15 @@
-package controller;
+/*
+    The mainController class is a "administrator only" controller. While the
+    same functionality is not available to the actual users of the application, 
+    it has been pivotal in designing and testing as many of the functions 
+    were able to be tested and created here before being passed onto the 
+    buyerController and sellerController
 
-/**
- * Sample Skeleton for 'mainView.fxml' Controller Class
- */
+    The only funcitonality unique to mainController is the scraper, which is
+    currently unfinished.
+*/
+
+package controller;
 
 import java.io.IOException;
 import java.net.URL;
@@ -36,148 +43,148 @@ public class mainController {
     private URL location;
 
     @FXML // fx:id="carPicture"
-    private ImageView carPicture; // Value injected by FXMLLoader
+    private ImageView carPicture;
 
     @FXML // fx:id="accountButton"
-    private Button accountButton; // Value injected by FXMLLoader
+    private Button accountButton;
 
     @FXML // fx:id="portfolioButton"
-    private Button portfolioButton; // Value injected by FXMLLoader
+    private Button portfolioButton;
 
     @FXML // fx:id="messagesButton"
-    private Button messagesButton; // Value injected by FXMLLoader
+    private Button messagesButton;
 
     @FXML // fx:id="horizontalSeparator"
-    private Separator horizontalSeparator; // Value injected by FXMLLoader
+    private Separator horizontalSeparator;
 
     @FXML // fx:id="searchCarLabel"
-    private Label searchCarLabel; // Value injected by FXMLLoader
+    private Label searchCarLabel;
 
     @FXML // fx:id="sellCarLabel"
-    private Label sellCarLabel; // Value injected by FXMLLoader
+    private Label sellCarLabel;
 
     @FXML // fx:id="vSeparator"
-    private Separator vSeparator; // Value injected by FXMLLoader
+    private Separator vSeparator;
 
     @FXML // fx:id="searchTermField"
-    private TextField searchTermField; // Value injected by FXMLLoader
+    private TextField searchTermField;
 
     @FXML // fx:id="initializePortfolioButton"
-    private Button initializePortfolioButton; // Value injected by FXMLLoader
+    private Button initializePortfolioButton;
 
     @FXML // fx:id="advancedSearchButton"
-    private Button advancedSearchButton; // Value injected by FXMLLoader
+    private Button advancedSearchButton;
 
     @FXML // fx:id="launchScraperButton"
-    private Button launchScraperButton; // Value injected by FXMLLoader
+    private Button launchScraperButton;
 
     @FXML // fx:id="searchButton"
-    private Button searchButton; // Value injected by FXMLLoader
+    private Button searchButton;
 
     @FXML // fx:id="makeField"
-    private TextField makeField; // Value injected by FXMLLoader
+    private TextField makeField;
 
     @FXML // fx:id="modelField"
-    private TextField modelField; // Value injected by FXMLLoader
+    private TextField modelField;
 
     @FXML // fx:id="vinField"
-    private TextField vinField; // Value injected by FXMLLoader
+    private TextField vinField;
 
     @FXML // fx:id="milageField"
-    private TextField mileageField; // Value injected by FXMLLoader
+    private TextField mileageField;
 
     @FXML // fx:id="makeLabel"
-    private Label makeLabel; // Value injected by FXMLLoader
+    private Label makeLabel;
 
     @FXML // fx:id="modelLabel"
-    private Label modelLabel; // Value injected by FXMLLoader
+    private Label modelLabel;
 
     @FXML // fx:id="vinLabel"
-    private Label vinLabel; // Value injected by FXMLLoader
+    private Label vinLabel;
 
     @FXML // fx:id="mileageLabel"
-    private Label mileageLabel; // Value injected by FXMLLoader
+    private Label mileageLabel;
 
     @FXML // fx:id="carfolioTitle"
-    private Label carfolioTitle; // Value injected by FXMLLoader
+    private Label carfolioTitle;
 
     @FXML // fx:id="greeting"
-    private Label greeting; // Value injected by FXMLLoader
+    private Label greeting;
 
     @FXML // fx:id="searchByMenu"
-    private MenuButton searchByMenu; // Value injected by FXMLLoader
+    private MenuButton searchByMenu;
 
     @FXML // fx:id="searchMake"
-    private MenuItem searchMake; // Value injected by FXMLLoader
+    private MenuItem searchMake;
 
     @FXML // fx:id="searchModel"
-    private MenuItem searchModel; // Value injected by FXMLLoader
+    private MenuItem searchModel;
 
     @FXML // fx:id="searchMileage"
-    private MenuItem searchMileage; // Value injected by FXMLLoader
+    private MenuItem searchMileage;
 
     @FXML // fx:id="searchStyle"
-    private MenuItem searchStyle; // Value injected by FXMLLoader
+    private MenuItem searchStyle;
 
     @FXML // fx:id="searchYear"
-    private MenuItem searchYear; // Value injected by FXMLLoader
+    private MenuItem searchYear;
 
-    private EntityManager em;
     
+    // Used for finding the current user from username given by loginController
+    private EntityManager em;
+    // The logged-in user
     private User activeUser;
+    // Car made form attributes in the "Sell a Car" section
     private Car sellCar;
     
-
+    
+    /* Opens the scraperView in package view in a new window when the 
+       launchScraperButton is clicked */
     @FXML
-    void beginSearch(ActionEvent event) {
+    void openScraper(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/scraperView.fxml"));
+        Parent scraperView = loader.load();
+        Scene scraperViewScene = new Scene(scraperView);        
+        Stage stage = new Stage();
+        stage.setScene(scraperViewScene);
+        stage.show();
+    }    
 
-    }
-
-    @FXML
-    void initializePortfolio(ActionEvent event) {
-        
-    }
-
+    /* Opens the accountView in the current window when accountButton is 
+    clicked. Also passes the activeUser to the accountController and sets the 
+    previous scene as the curent one (mainView)*/     
     @FXML
     void openAccount(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/accountView.fxml"));
         Parent accountView = loader.load();
         Scene accountViewScene = new Scene(accountView);
-        accountController controller = loader.getController();
-        controller.setActiveUser(activeUser);
-        controller.setPreviousScene(((Node) event.getSource()).getScene());
+        accountController aController = loader.getController();
+        aController.setActiveUser(activeUser);
+        aController.setPreviousScene(((Node) event.getSource()).getScene());
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(accountViewScene);
         window.show();
     }
-
-    @FXML
-    void openAdvancedSearch(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/searchView.fxml"));
-        Parent searchView = loader.load();
-        Scene searchViewScene = new Scene(searchView);
-        searchController controller = loader.getController();
-        controller.setActiveUser(activeUser);
-        controller.setPreviousScene(((Node) event.getSource()).getScene());
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(searchViewScene);
-        window.show();
-
-    }
-
+    
+    /* Opens the messageView in the current window when messagesButton is 
+    clicked. Also passes the activeUser to the messageController and sets 
+    the previous scene as the curent one (mainView)*/    
     @FXML
     void openMessages(ActionEvent event) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/messageView.fxml"));
         Parent messageView = loader.load();
         Scene messageViewScene = new Scene(messageView);
-        messageController controller = loader.getController();
-        controller.setActiveUser(activeUser);
-        controller.setPreviousScene(((Node) event.getSource()).getScene());
+        messageController mController = loader.getController();
+        mController.setActiveUser(activeUser);
+        mController.setPreviousScene(((Node) event.getSource()).getScene());
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(messageViewScene);
         window.show();
-    }
-
+    }    
+    
+    /* Opens the userPortfolioView in the current window when portfolioButton 
+    is clicked. Also passes the activeUser to the userPortfolioController and
+    sets the previous scene as the curent one (mainView)*/
     @FXML
     void openPortfolios(ActionEvent event) throws IOException {
         FXMLLoader userPortfolioLoader = new FXMLLoader(getClass().getResource("/view/userPortfolioView.fxml"));
@@ -189,82 +196,113 @@ public class mainController {
         Stage userPortfolioWindow = (Stage) ((Node) event.getSource()).getScene().getWindow();
         userPortfolioWindow.setScene(userPortfolioUI);
         userPortfolioWindow.show();
-    }
-
-    @FXML
-    void openScraper(ActionEvent event) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/scraperView.fxml"));
-        Parent scraperView = loader.load();
-        Scene scraperViewScene = new Scene(scraperView);
-        scraperController controller = loader.getController();
-        
-        Stage stage = new Stage();
-        stage.setScene(scraperViewScene);
-        stage.show();
-    }
-
-    @FXML
-    void setMake(KeyEvent event) {
-        sellCar.setMake(makeField.getText());
-    }
-
+    }    
+    
+    // Sets the text of searchByMenu to Make when searchMake is selected
     @FXML
     void setMakeSearch(ActionEvent event) {
         searchByMenu.setText("Make");
-    }
-
+    }    
+   
+    // Sets the text of searchByMenu to Model when searchModel is selected
     @FXML
-    void setMileage(KeyEvent event) {
-        sellCar.setMiles(Integer.valueOf(mileageField.getText()));
+    void setModelSearch(ActionEvent event) {
+        searchByMenu.setText("Model");
     }
-
+    
+    // Sets the text of searchByMenu to Mileage when searchMiles is selected
     @FXML
     void setMileageSearch(ActionEvent event) {
         searchByMenu.setText("Mileage");
     }
+    
+    // Sets the text of searchByMenu to style when searchStyle is selected
+    @FXML
+    void setStyleSearch(ActionEvent event) {
+        searchByMenu.setText("Style");
+    }
+    
+    // Sets the text of searchByMenu to Year when searchYear is selected
+    @FXML
+    void setYearSearch(ActionEvent event) {
+        searchByMenu.setText("Year");
+    }
+    
+    // TODO: Entering a search term
+    @FXML
+    void setSearch(KeyEvent event) {
 
+    }
+    
+    /* Opens the searchView in the current window when advancedSearchButton is 
+    clicked. Also passes the activeUser to the searchController and sets the 
+    previous scene as the curent one (mainView)*/ 
+    @FXML
+    void openAdvancedSearch(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/searchView.fxml"));
+        Parent searchView = loader.load();
+        Scene searchViewScene = new Scene(searchView);
+        searchController sController = loader.getController();
+        sController.setActiveUser(activeUser);
+        sController.setPreviousScene(((Node) event.getSource()).getScene());
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(searchViewScene);
+        window.show();
+
+    }
+    
+    // TODO: Add search function
+    @FXML
+    void beginSearch(ActionEvent event) {
+
+    }
+
+    // TODO: Create portfolio function
+    @FXML
+    void initializePortfolio(ActionEvent event) {
+        
+    }
+    
+    // Sets the make of sellCar to the text in makeField when a key is pressed            
+    @FXML
+    void setMake(KeyEvent event) {
+        sellCar.setMake(makeField.getText());
+    }
+    
+    // Sets the model of sellCar to the text in modelField when a key is pressed    
     @FXML
     void setModel(KeyEvent event) {
         sellCar.setMake(makeField.getText());
     }
 
-    @FXML
-    void setModelSearch(ActionEvent event) {
-        searchByMenu.setText("Model");
-    }
-
-    @FXML
-    void setSearch(KeyEvent event) {
-
-    }
-
-    @FXML
-    void setStyleSearch(ActionEvent event) {
-        searchByMenu.setText("Style");
-    }
-
+    // Sets the vin of sellCar to the text in vinField when a key is pressed
     @FXML
     void setVin(KeyEvent event) {
         sellCar.setVin(vinField.getText());
     }
-
+    
+    // Sets the miles of sellCar to the text in mileageField when a key is pressed    
     @FXML
-    void setYearSearch(ActionEvent event) {
-        searchByMenu.setText("Year");
+    void setMileage(KeyEvent event) {
+        sellCar.setMiles(Integer.valueOf(mileageField.getText()));
     }
 
+
+
+    // Function used for setting the User activeUser when directed to this page
     public void setActiveUser(User fromLogin) {
         //activeUser = fromLogin;
         String queryUsername = fromLogin.getUsername();
         activeUser = em.find(User.class, queryUsername);
-    }
+    }    
     
+    // Function used for setting the label Greeting as text specified in login
     public void setGreeting(String greet) {
         this.greeting.setText(greet);
     }
     
 
-
+    
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert carPicture != null : "fx:id=\"carPicture\" was not injected: check your FXML file 'mainView.fxml'.";
